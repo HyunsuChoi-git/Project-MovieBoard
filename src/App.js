28c336ac6c6d42bd21979c96fad49bfc570b3216
HyunsuChoi-git <choihs1054@gmail.com>
2022-07-05 14:22:55 +0900

    study useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // let number = 1; //상태값 아님
-  const [number, setNumber] = useState(1);  // 상태값임
-  // 리액트가 상태값이 변경된 것을 알아차릴 수 있도록 React의 hooks 라이브러리 사용
-  // useState를 이용해 값을 number에 넣어줌.
-  // 값변경 시에는 setNumber을 이용해서 리엑트한테 number값 변경을 요청하는 형태임.
-  // setNumber가 호출되면 리엑트는 랜더링을 실시함.
+  console.log("App 실행됨");
 
-  const add = () => {
-    setNumber(number+1); 
-    console.log('add', number);
-  };
+  let sample = [
+    {id:1, name:"홍길동"},
+    {id:2, name:"홍길순"},
+    {id:3, name:"홍길훈"},
+    {id:4, name:"홍길영"},
+  ]
 
-  //랜더링 시점 = 상태값 변경
+  const [users, setUsers] = useState(sample);
+
+  const download = () => {
+    const sample2 = [...sample, {id:5, name:"홍길자"}]
+    setUsers(sample2);
+  }
+
+  //랜더링 시점 = 상태값 변경 => 레퍼런스 변경
   return (
     <div>
-      <div>숫자 : {number}</div>
-      <button onClick={add}>더하기</button>
+      <button onClick={download}>다운로드</button>
+      {users.map(u=>
+        <h1>
+          {u.id} / {u.name}
+        </h1>
+      )}
     </div>
     
   );
--- src/Wood.js
+++ src/Wood.js
@@ -4,7 +4,7 @@ function Wood() {
 
   return (
     <div>
-      <h1 className='box-style'>나무입니다.</h1>
+      <h1 className='box-style'>나무입니다!!</h1>
     </div>
     
   );
