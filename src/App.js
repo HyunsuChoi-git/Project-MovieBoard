2a7768440c3ba50bdb478dbaaee45c0a15a35cf9
HyunsuChoi-git <choihs1054@gmail.com>
2022-07-07 17:34:25 +0900

    study styled-components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import { createRef, useMemo, useRef, useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import { Title } from './MyCss';
+
+//h1태그를 가상으로 만드는 것 
+//JS 문법 --> let T = document.createElement("h1"); 
+/*
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
+*/
+
 
 function App() {
-
-  const myRef = useRef(null);
-
-  const [list, setList] = useState([{id:1,name:"길동"}, {id:2,name:"꺽정"}])
-
-  // list 길이 만큼 myRef를 생성하여 리스트에 저장
-  const myRefs = Array.from({length:list.length}).map(() => createRef());
-
   return (
     <div>
-      <button onClick={()=> {
-        console.log(myRef);
-        console.log(myRef.current);  // <div>박스</div>
-        console.log(list);
-        myRef.current.style.backgroundColor='red';
-        myRefs[0].current.style.backgroundColor='green';
-        myRefs[1].current.style.backgroundColor='yellow';
-      }}>색변경</button>
-      <div ref={myRef}>박스</div>
-
-      {list.map((user, index)=>(
-        <h1 ref={myRefs[index]}>{user.id}. {user.name}</h1>
-      ))}
+      <Title>안녕</Title>
     </div>
-
-
-
     
   );
 }
