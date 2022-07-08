bdf38feb00754e8f47eeba2d819a7a0ded99a5f3
HyunsuChoi-git <choihs1054@gmail.com>
2022-07-08 13:42:11 +0900

    study styled component passing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-//재사용을 위한 하나의 컴포넌트를 생성할 때
-//css와 js 파일이 하나로 되어있어 관리가 편하다!
-//디자인도 컴포넌트화된다!
-//js에 디자인을 넣으면서 css문법을 가져다 쓸 수 있다.
-//디자인 적용시에 <태그>로써 사용할 수 있다.
 
-const HeaderList = styled.div`
+const StyledHEaderDiv = styled.div`
     border: 1px solid black;
     height: 300px;
+    background-color: ${(props)=>props.backgroundColor};
 `
 
 const Header = () => {
     return (
-        <HeaderList>
+        <StyledHEaderDiv backgroundColor='skyblue'>
             <ul>
                 <li>소개</li>
                 <li>News</li>
             </ul>
-        </HeaderList>
+        </StyledHEaderDiv>
     );
 };
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+let StyledDeleteButton = styled.button`
+color : ${(props) => props.user.name === 'hera' ? 'blue' : 'yellow'};
+`;
 
 
 const Home = (props) => {
@@ -6,14 +12,11 @@ const Home = (props) => {
     console.log(props.boards);
 
     //구조분할할당
-    const { boards, setBoards, number, setNumber } = props;
-    // { } 내부에, 넘어온 props명을 변수명으로 명시해주면 알아서 props내부에 있는 해당 데이터가 {}에 명시한 변수에 들어간다. {}내부는 ','로 복수개 가능
+    const { boards, setBoards, user, setUser} = props;
 
     return (
         <div>
-          <h1>홈: {number}</h1>
-          <button onClick={() => setNumber(number+1)}>번호 증가</button>
-          <button onClick={() => setBoards([])}>전체삭제</button>
+          <StyledDeleteButton user={user} onClick={() => setUser({...user, name:'su'})}>전체삭제</StyledDeleteButton>
           {boards.map((board)=>(
             <h3>제목: {board.title}, 내용: {board.content} </h3>
           ))}
